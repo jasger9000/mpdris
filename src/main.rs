0aee6d2bf968f83b7490ce6f06f71b6e48d265e6
J. Gerhards <g1.jasger@gmail.com>
2024-05-21 14:12:52 +0200

    add daemonize functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 mod config;
 mod connection;
 
+use clap::{arg, value_parser, Command};
+use libc::{
+    sighandler_t, signal, EXIT_SUCCESS, SIGHUP, SIGTERM,
+    SIG_ERR, STDERR_FILENO, STDIN_FILENO, STDOUT_FILENO,
+};
+use std::cmp::Ordering;
 use std::env;
+use std::ffi::CString;
 use std::net::IpAddr;
 use std::path::PathBuf;
-use clap::{arg, Command, value_parser};
+use std::process::exit;
 
 use crate::config::Config;
 use crate::connection::MpdConnection;
@@ -12,6 +19,7 @@ use crate::connection::MpdConnection;
 #[rustfmt::skip]
 const VERSION_STR: &str = concat!("v", env!("CARGO_PKG_VERSION"), " (", env!("GIT_HASH"), ") compiled using rustc v", env!("RUSTC_VERSION"));
 
+#[cfg(target_os = "linux")]
 fn main() {
     #[cfg(not(debug_assertions))]
     let config_path: PathBuf = {
@@ -39,7 +47,17 @@ fn main() {
         .arg(arg!(-p --port <PORT> "The port over which to connect to mpd").value_parser(value_parser!(u16)))
         .arg(arg!(-a --addr <ADDRESS> "the ip address over which to connect to mpd"))
         .arg(arg!(--"no-spawn-daemon" "When set does not try to fork into a daemon"))
+        .arg(arg!(--"systemd" "When set acts as a daemon without forking the process"))
         .get_matches();
+
+    {
+        let is_daemon = !matches.get_flag("no-spawn-daemon") || matches.get_flag("systemd");
+        let should_fork = !matches.get_flag("no-spawn-daemon");
+
+        if should_fork {
+            daemonize();
+        }
+    }
 
     let mut config = {
         match Config::load_config(config_path.as_path()) {
@@ -49,7 +67,7 @@ fn main() {
             }
         }
     };
-    
+
     if !config_path.is_file() {
         match config.write(&config_path) {
             Ok(_) => {},
@@ -59,7 +77,6 @@ fn main() {
 
     if let Some(port) = matches.get_one::<u16>("port") { config.port = *port; }
     if let Some(addr) = matches.get_one::<IpAddr>("addr") { config.addr = *addr; }
-    let spawn_daemon = !matches.get_flag("no-spawn-daemon");
 
     let mut conn = match MpdConnection::init_connection(config.addr, config.port) {
         Ok(c) => c,
@@ -67,4 +84,34 @@ fn main() {
             panic!("Could not connect to mpd server: {e}")
         }
     };
+}
+
+fn daemonize() {
+    let pid = unsafe { libc::fork() };
+    match pid.cmp(&0) {
+        Ordering::Less => {
+            panic!("Could not fork the process")
+        }
+        Ordering::Equal => {} // child process
+        Ordering::Greater => exit(EXIT_SUCCESS),
+    }
+
+    let sid = unsafe { libc::setsid() };
+    if sid < 0 {
+        panic!("Could not create a new session for the daemon");
+    }
+
+    let root_dir = CString::new("/").expect("Root path descriptor creation failed");
+    if unsafe { libc::chdir(root_dir.as_ptr()) } < 0 {
+        panic!("Could not change to root directory");
+    }
+
+    unsafe {
+        if libc::close(STDIN_FILENO) < 0
+            || libc::close(STDOUT_FILENO) < 0
+            || libc::close(STDERR_FILENO) < 0
+        {
+            panic!("Could not filedescriptors stdin, stdout, stderr");
+        }
+    }
 }
