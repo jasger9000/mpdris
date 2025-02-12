4cf6c73c8b7a64bc2509702c0cd258393f1af4fb
J. Gerhards <g1.jasger@gmail.com>
2025-02-12 23:09:09 +0100

    make use of map_err() instead of map where possible
    When we just need to early-return a mapped error there's no need to use
    a match statement for that.

--- src/config.rs
+++ src/config.rs
@@ -76,10 +76,7 @@ impl Config {
             fs::create_dir(file.parent().unwrap()).await?;
         }
 
-        let data = match toml::to_string_pretty(self) {
-            Ok(d) => d,
-            Err(err) => return Err(io::Error::new(io::ErrorKind::InvalidData, err.to_string())),
-        };
+        let data = toml::to_string_pretty(self).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
 
         fs::write(file, data).await?;
 
@@ -131,27 +128,21 @@ impl Config {
     /// Loads values $MPD_HOST and $MPD_PORT from environment
     fn load_from_env_vars(&mut self) -> io::Result<()> {
         if let Ok(addr) = env::var("MPD_HOST") {
-            self.addr = match addr.parse() {
-                Ok(a) => a,
-                Err(_) => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "Could not parse the $MPD_HOST environment variable into a host address.",
-                    ))
-                }
-            }
+            self.addr = addr.parse().map_err(|_e| {
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "Could not parse the $MPD_HOST environment variable into a host address.",
+                )
+            })?;
         }
 
         if let Ok(port) = env::var("MPD_PORT") {
-            self.port = match port.parse() {
-                Ok(p) => p,
-                Err(_) => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "Could not parse the $MPD_PORT environment variable into an integer.",
-                    ))
-                }
-            }
+            self.port = port.parse().map_err(|_e| {
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "Could not parse the $MPD_PORT environment variable into an integer.",
+                )
+            })?;
         }
 
         Ok(())
@@ -161,12 +152,7 @@ impl Config {
     async fn load_from_file(file: &Path) -> io::Result<Self> {
         let data = fs::read_to_string(file).await?;
 
-        match toml::from_str(&data) {
-            Ok(config) => Ok(config),
-            Err(err) => {
-                return Err(io::Error::new(io::ErrorKind::InvalidData, err.message()));
-            }
-        }
+        toml::from_str::<Config>(&data).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.message()))
     }
 }
 
