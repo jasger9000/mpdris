d7be501e694ab95a54b7e2e34f9fdd40df8e637d
J. Gerhards <g1.jasger@gmail.com>
2025-02-20 15:39:56 +0100

    Use PathBuf for Song.uri
    Use PathBuf for Song.uri in favour of Arc<str> since it is a file path
    and we need to format it into a String anyways when we output the
    metadata, so there's no disadvantage to doing this.

--- src/client/status.rs
+++ src/client/status.rs
@@ -1,8 +1,6 @@
 use async_std::channel::Sender;
 use log::debug;
-use std::mem::replace;
-use std::sync::Arc;
-use std::time::Duration;
+use std::{mem::replace, path::PathBuf, sync::Arc, time::Duration};
 
 use crate::config::config;
 
@@ -65,7 +63,7 @@ pub enum Repeat {
 
 #[derive(Debug, Clone)]
 pub struct Song {
-    pub uri: Arc<str>,
+    pub uri: PathBuf,
     pub cover: Option<Arc<str>>,
     pub artists: Vec<Arc<str>>,
     pub album: Option<Arc<str>>,
@@ -91,7 +89,7 @@ impl Song {
     /// Creates a new empty song
     pub fn new() -> Self {
         Self {
-            uri: "".into(),
+            uri: PathBuf::new(),
             cover: None,
             artists: Vec::new(),
             album: None,
@@ -108,7 +106,7 @@ impl Song {
     }
 
     async fn try_set_cover_url(&mut self) {
-        debug!("searching cover for '{}'", self.uri);
+        debug!("searching cover for '{}'", self.uri.display());
 
         let paths = {
             let c = config().read().await;
